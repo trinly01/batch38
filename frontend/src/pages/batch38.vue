d6851a37f956f41ff92728f46c5c85fd69d1fb89
Trinmar Boado <timi.boado@gmail.com>
2022-03-07 21:15:36 +0800

    Initial commit

---
+++ frontend/src/pages/batch38.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-toolbar class="bg-purple text-white">
+    <q-btn flat round dense icon="assignment_ind" />
+    <q-toolbar-title>
+      Batch 38 Todo App
+    </q-toolbar-title>
+    <q-btn flat round dense icon="apps" class="q-mr-xs" />
+    <q-btn flat round dense icon="more_vert" />
+  </q-toolbar>
+  <q-input filled label="Filled" />
+  <button>Add</button>
+  <ul>
+    <li>asdasd</li>
+    <li>asdasd</li>
+    <li>asdasd</li>
+    <li>asdasd</li>
+    <li>asdasd</li>
+  </ul>
+</template>
