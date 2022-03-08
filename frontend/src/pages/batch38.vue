5eb1b662fa21fea1c5222b6ebcb1a2aa5cafc796
Trinmar Boado <timi.boado@gmail.com>
2022-03-08 20:02:06 +0800

    Add functionality ref vs reactive

--- frontend/src/pages/batch38.vue
+++ frontend/src/pages/batch38.vue
@@ -3,30 +3,50 @@
     <q-toolbar class="bg-purple text-white">
       <q-btn flat round dense icon="assignment_ind" />
       <q-toolbar-title>
+        {{ state.task }}
         Batch 38 Todo App
       </q-toolbar-title>
       <q-btn flat round dense icon="apps" class="q-mr-xs" />
       <q-btn flat round dense icon="more_vert" />
     </q-toolbar>
     <div class="row q-gutter-sm q-pa-sm" >
-      <q-input class="col" filled label="What needs to be done" />
-      <q-btn icon="add" label="add" />
+      <q-input v-model="state.task" class="col" filled label="What needs to be done" />
+      <q-btn @click="add" icon="add" label="add" />
     </div>
-    <q-list class="q-ma-sm" bordered separator>
+    <q-list v-for="todo in todos" :key="todo.id" class="q-ma-sm" bordered separator>
       <q-item clickable v-ripple>
         <q-item-section avatar>
           <q-icon name="signal_wifi_off" />
         </q-item-section>
-        <q-item-section>Active</q-item-section>
-        <q-item-section side>Side</q-item-section>
-      </q-item>
-      <q-item clickable v-ripple>
-        <q-item-section avatar>
-          <q-icon name="signal_wifi_off" />
-        </q-item-section>
-        <q-item-section>Active</q-item-section>
+        <q-item-section>{{ todo.desc }}</q-item-section>
         <q-item-section side>Side</q-item-section>
       </q-item>
     </q-list>
   </div>
 </template>
+
+<script setup>
+
+import { reactive, ref } from 'vue'
+
+const state = reactive({
+  task: ''
+})
+
+// const task = ref('')
+const todos = ref([
+  {
+    id: Date.now(),
+    desc: 'Add Function',
+    isDone: false
+  }
+])
+
+function add () {
+  todos.value.unshift({
+    id: Date.now(),
+    desc: state.task,
+    isDone: false
+  })
+}
+</script>
